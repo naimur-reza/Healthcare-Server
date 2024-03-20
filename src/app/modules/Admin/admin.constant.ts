c7a5da3b23f8e09cbcb897de1c6b81375568ae09
Naimur Reza <naimur.rezaa@gmail.com>
2024-03-20 23:45:34 +0600

    prevent unknown filtering by pick

---
+++ src/app/modules/Admin/admin.constant.ts
@@ -0,0 +1,1 @@
+export const adminFilterableFields = ["name", "email"];
--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,14 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllAdmins = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllAdminsFormDB(req.query);
+  const pick = <T extends Record<string, unknown>, K extends keyof T>(
+    obj: T,
+    fields: K[],
+  ) => {
+    const finalObj: Partial<T> = {};
+    for (const key of fields) {
+      if (obj && Object.prototype.hasOwnProperty.call(obj, key)) {
+        finalObj[key] = obj[key];
+      }
+    }
+    return finalObj;
+  };
 
-  res.status(200).send({
-    success: true,
-    message: "Admins retrieved successfully!",
-    data: result,
-  });
+  try {
+    const filter = pick(req.query, adminFilterableFields);
+
+    const result = await AdminService.getAllAdminsFormDB(filter);
+
+    res.status(200).send({
+      success: true,
+      message: "Admins retrieved successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: true,
+      message: error.message || "Something went wrong!",
+      data: null,
+    });
+  }
 };
 
 export const AdminController = { getAllAdmins };
