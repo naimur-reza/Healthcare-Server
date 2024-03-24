0e00903b230cd836438ba9d9aad9199b3185bb1a
Naimur Reza <naimur.rezaa@gmail.com>
2024-03-25 02:32:52 +0600

    added stack in generic error

--- src/app/errors/GenericError.ts
+++ src/app/errors/GenericError.ts
@@ -1,11 +1,13 @@
 class GenericError extends Error {
   statusCode: number;
   message: string;
+  stack?: string | undefined;
 
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
     this.message = message;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
