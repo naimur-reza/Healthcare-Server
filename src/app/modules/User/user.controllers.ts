598594c34e2968d0f9b3c65677747fa6444eeca7
Naimur Reza <naimur.rezaa@gmail.com>
2024-03-28 22:38:22 +0600

    Add user retreival endpoint

--- src/app/modules/User/user.controllers.ts
+++ src/app/modules/User/user.controllers.ts
@@ -2,14 +2,19 @@ import { Request, Response } from "express";
 import { userServices } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
 import catchAsync from "../../utils/catchAsync";
+import { pick } from "../../shared/pick";
+import { userFilterableFields } from "./user.constant";
 
 const getAllUsers = catchAsync(async (req: Request, res: Response) => {
-  const users = await userServices.getAllUsersFromDB();
+  const filter = pick(req.query, userFilterableFields);
+  const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+  const { data, meta } = await userServices.getAllUsersFromDB(filter, options);
   sendResponse(res, {
     statusCode: 200,
     success: true,
     message: "Users retrieved successfully!",
-    data: users,
+    data,
+    meta,
   });
 });
 
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,16 +1,73 @@
-import { userRole, UserStatus } from "@prisma/client";
+import { Prisma, userRole } from "@prisma/client";
 import bcrypt from "bcrypt";
 import prisma from "../../shared/prisma";
 import { sendImageToCloudinary } from "../../helpers/fileUploader";
 import { UploadApiResponse } from "cloudinary";
 import { IAdmin, IDoctor } from "./user.interface";
+import { calculatePagination } from "../../helpers/paginationHelper";
+import { IOptions, IParams } from "../Admin/admin.interface";
+import { userSearchAbleFields } from "./user.constant";
 
-const getAllUsersFromDB = async () => {
-  const users = await prisma.user.findMany({
-    where: { status: UserStatus.ACTIVE },
-    // select: { password: false },
+const getAllUsersFromDB = async (params: IParams, options: IOptions) => {
+  const { searchTerm, ...filterData } = params;
+  const { limit, skip } = calculatePagination(options);
+
+  const andOption: Prisma.UserWhereInput[] = [];
+
+  if (searchTerm) {
+    andOption.push({
+      OR: userSearchAbleFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andOption.push({
+      AND: Object.keys(filterData).map(key => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereCondition: Prisma.UserWhereInput =
+    andOption.length > 0 ? { AND: andOption } : {};
+
+  const data = await prisma.user.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "asc",
+          },
+    select: {
+      id: true,
+      email: true,
+      role: true,
+      needPasswordChange: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   });
-  return users;
+
+  const meta = {
+    page: Number(options.page) || 1,
+    limit,
+    total: data.length,
+  };
+
+  return { data, meta };
 };
 
 const createAdmin = async (file: any, data: IAdmin) => {
