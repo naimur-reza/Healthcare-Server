89c572521cf6bb7bb9a42090aa9466ba4fe4e200
Naimur Reza <naimur.rezaa@gmail.com>
2024-04-17 12:45:24 +0600

    Add schedule get by id and delete

--- src/app/modules/Schedule/schedule.controller.ts
+++ src/app/modules/Schedule/schedule.controller.ts
@@ -3,8 +3,9 @@ import sendResponse from "../../utils/sendResponse";
 import { scheduleService } from "./schedule.service";
 import { pick } from "../../shared/pick";
 import { scheduleFilterableFields } from "./schedule.constant";
+import catchAsync from "../../utils/catchAsync";
 
-const createSchedule = async (req: Request, res: Response) => {
+const createSchedule = catchAsync(async (req: Request, res: Response) => {
   const patient = await scheduleService.insertIntoDB(req.body);
   sendResponse(res, {
     statusCode: 200,
@@ -12,9 +13,9 @@ const createSchedule = async (req: Request, res: Response) => {
     message: "Schedule created successfully!",
     data: patient,
   });
-};
+});
 
-const getAllSchedule = async (req: Request, res: Response) => {
+const getAllSchedule = catchAsync(async (req: Request, res: Response) => {
   const filterParams = pick(req.query, scheduleFilterableFields);
   const options = pick(req.query, ["limit", "sortBy", "sortOrder", "page"]);
   const user = req.user;
@@ -31,9 +32,33 @@ const getAllSchedule = async (req: Request, res: Response) => {
     meta,
     data: result,
   });
-};
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await scheduleService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Schedule retrieval successfully",
+    data: result,
+  });
+});
+
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await scheduleService.deleteFromDB(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Schedule deleted successfully",
+    data: result,
+  });
+});
 
 export const scheduleController = {
   createSchedule,
   getAllSchedule,
+  getByIdFromDB,
+  deleteFromDB,
 };
--- src/app/modules/Schedule/schedule.routes.ts
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -9,11 +9,25 @@ const router = Router();
 
 router.get("/", checkAuth(userRole.DOCTOR), scheduleController.getAllSchedule);
 
+router.get("/:id", checkAuth(userRole.DOCTOR));
+
+router.get(
+  "/:id",
+  checkAuth(userRole.SUPER_ADMIN, userRole.ADMIN, userRole.DOCTOR),
+  scheduleController.getByIdFromDB,
+);
+
 router.post(
   "/",
   checkAuth(userRole.DOCTOR),
   validateRequest(scheduleValidation.scheduleSchema),
   scheduleController.createSchedule,
+);
+
+router.delete(
+  "/:id",
+  checkAuth(userRole.SUPER_ADMIN, userRole.ADMIN),
+  scheduleController.deleteFromDB,
 );
 
 export const scheduleRoutes = router;
